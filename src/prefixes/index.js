1b559a5699af6a760f15117bfda45efa2beeba0f
JP BLOND <jpb.blond@gmail.com>
2020-03-14 00:48:37 +0100

    adding prefixes

--- src/prefixes/index.js
+++ src/prefixes/index.js
@@ -5,3 +5,30 @@ export const controllerPrefixes = [
   "update",
   "delete"
 ];
+
+export const useCasePrefixes = [
+  "add",
+  "find-one",
+  "find-all",
+  "edit",
+  "remove"
+];
+
+// to specify
+export const entityPrefixes = [];
+
+export const dataAccessPrefixes = [
+  "insert",
+  "find-one", // maybe "select" to respect SQL vocabulary
+  "find-all",
+  "update",
+  "delete"
+];
+
+export const keys = {
+  creating: 0,
+  gettingOne: 1,
+  gettingAll: 2,
+  updating: 3,
+  removing: 4
+};
