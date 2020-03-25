b9106d2219792d7374d70df247e3d02a5cb6aa19
JP BLOND <jpb.blond@gmail.com>
2020-03-25 19:43:09 +0100

    integrate appRoot

--- src/generate-module/index.js
+++ src/generate-module/index.js
@@ -1,5 +1,9 @@
 // deps
 const sourceDir = "/home/jp_blond/Projects/perso/clean-cli/tmp";
+
+const appRoot = require("app-root-path");
+console.log("appRoot", appRoot.path);
+
 import createFile from "../utils/create-file";
 import { generateControllerCode } from "../generate-template";
 
