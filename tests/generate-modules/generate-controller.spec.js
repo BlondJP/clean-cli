b6a94f003d3ccdaf29c480683f06897ccd12bfd5
JP BLOND <jpb.blond@gmail.com>
2020-04-17 15:33:37 +0200

    integrating test of generate controller

--- tests/generate-modules/generate-controller.spec.js
+++ tests/generate-modules/generate-controller.spec.js
@@ -1,5 +1,8 @@
 import faker from "faker";
+import fs from "fs";
 import makeGenerateController from "../../src/generate-module/generate-controller";
+import appRoot from "app-root-path";
+import { checkFolderExist, createFile } from "../../src/utils";
 
 describe("testing generate controller", () => {
   it("generate a controller succesfuly full mock", async () => {
@@ -27,5 +30,30 @@ describe("testing generate controller", () => {
     expect(checkFolderExist).toHaveBeenCalledWith(`${sourceDir}/controllers`);
     expect(createTemplate).toHaveBeenCalledWith(moduleName, prefix);
     expect(createFile).toHaveBeenCalledWith(filePath, template);
+  });
+
+  it("generate a controller succesfuly creating the file", async () => {
+    // mocks
+    const rootDir = appRoot.path;
+    const sourceDir = rootDir + "/tmp";
+    const template = faker.random.words();
+    const createTemplate = jest.fn(() => template);
+
+    // params
+    const moduleName = faker.hacker.noun() + "-" + faker.hacker.noun();
+    const prefix = faker.random.word();
+
+    // usage
+    const generateController = makeGenerateController(
+      sourceDir,
+      createFile,
+      createTemplate,
+      checkFolderExist
+    );
+    const filePath = await generateController(moduleName, prefix);
+    expect(typeof filePath).toBe("string");
+
+    const content = await fs.promises.readFile(filePath, "utf-8");
+    expect(content).toBe(template);
   });
 });
