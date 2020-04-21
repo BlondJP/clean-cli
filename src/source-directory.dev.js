90d5ce3e46705ae6accac2ec4643f67ad69043d4
JP BLOND <jpb.blond@gmail.com>
2020-04-21 12:48:43 +0200

    dev env

---
+++ src/source-directory.dev.js
@@ -0,0 +1,7 @@
+import appRoot from "app-root-path";
+
+const projectRootDir = appRoot.path;
+const workingDir = "tmp";
+
+const sourceDirectory = `${projectRootDir}/${workingDir}`;
+export default sourceDirectory;
