20215de1752c61fd3b69fb9a1cfaa7ff2be04fb7
Jean-Philippe BLOND <jpb.blond@gmail.com>
2020-08-19 20:40:44 +0200

    POC commander

--- src/source-directory.js
+++
@@ -1,6 +0,0 @@
-import appRoot from "app-root-path";
-
-const sourceDirectory = appRoot.path; /** prod */
-// const sourceDirectory = appRoot.path + "/tmp"; // dev
-
-export default sourceDirectory;
