9782d889827cdbfe08807a991953a416d113f02a
Jean-Philippe BLOND <jean-philippe@seedger.com>
2020-09-18 17:46:32 +0200

    generate use cases

---
+++ src/generate-templates/generate-use-case-code.ts
@@ -0,0 +1,15 @@
+export default (convertToCamelCase, capitalizeFirstLetter) =>
+  function generateUseCaseCode(moduleName, prefix) {
+    const formatedPrefix = convertToCamelCase(prefix);
+    const camelCaseModuleName = convertToCamelCase(moduleName);
+
+    const formatedModuleName =
+      formatedPrefix.length > 0
+        ? capitalizeFirstLetter(camelCaseModuleName)
+        : camelCaseModuleName;
+
+    const template = `export default (${camelCaseModuleName}Db) => async function ${formatedPrefix}${formatedModuleName}(userInfos) {
+  }`;
+
+    return template;
+  };
