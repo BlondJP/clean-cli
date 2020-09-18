af2f1d591ef32c55323efd0eb6e2a6bcbfc11f7e
Jean-Philippe BLOND <jpb.blond@gmail.com>
2020-09-18 17:13:28 +0200

    apply typing in ts files

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 #! /usr/bin/env node
-const { Command } = require("commander");
-const { generateController } = require("./src/generate-modules");
+import { Command } from "commander";
+import { generateController } from "./src/generate-modules";
 
 const program = new Command();
 program.version("0.0.1");
 program
-  .option("-g --generate-file <fileType>", "type of the file we will generate")
+  .option("-g --file-type <fileType>", "type of the file we will generate")
   .option("-e --entity-name <entityName>", "name of the entity we will server")
   .option("-a --action-type <actionType>", "action the controller will do");
 program.parse(process.argv);
 
-const { generateFile: fileType, entityName, actionType } = program;
+const { fileType, entityName, actionType } = program;
 console.log({ fileType, entityName, actionType });
 
 if (fileType === "controller") {
