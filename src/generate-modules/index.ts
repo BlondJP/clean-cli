4f5048f88a0f2a18007dfae064ee72443ab60c48
Jean-Philippe BLOND <jpb.blond@gmail.com>
2020-09-19 02:05:29 +0200

    generate data access

--- src/generate-modules/index.ts
+++ src/generate-modules/index.ts
@@ -4,16 +4,18 @@ const sourceDir: string = `${currentDir()}/src`;
 
 import { actions, controllerPrefixes, useCasePrefixes } from "../prefixes";
 
-import { createFile, checkFolderExist } from "../utils";
+import { createFile, checkFolderExist, checkFileExist } from "../utils";
 
 import {
   generateControllerCode,
   generateUseCaseCode,
+  generateDataAccessCode,
 } from "../generate-templates";
 
 // factories
 import makeGenerateController from "./generate-controller";
 import makeGenerateUseCase from "./generate-use-case";
+import makeGenerateDataAccess from "./generate-data-access";
 
 // exports
 export const generateController = makeGenerateController(
@@ -32,4 +34,12 @@ export const generateUseCase = makeGenerateUseCase(
   checkFolderExist,
   useCasePrefixes,
   actions
+);
+
+export const generateDataAccess = makeGenerateDataAccess(
+  sourceDir,
+  createFile,
+  generateDataAccessCode,
+  checkFolderExist,
+  checkFileExist
 );
---
+++ src/generate-templates/generate-data-access-code.ts
@@ -0,0 +1,17 @@
+export default (
+  convertToCamelCase: (str: string) => string,
+  dataAccessPrefixes: string[],
+  actions: Object
+) =>
+  function generateDataAccessCode(action: string): string {
+    const functionName: string = dataAccessPrefixes[actions[action]];
+    const formatedFuncName: string = convertToCamelCase(functionName);
+
+    const template = `export default () => {
+        async function ${formatedFuncName}() {
+        }
+        return Object.freeze({${formatedFuncName}})
+    }`;
+
+    return template;
+  };
